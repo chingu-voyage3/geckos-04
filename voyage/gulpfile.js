de28711c80e2716e023e55b475c62fc1d50597f6
John <j24oh.uw@gmail.com>
2017-12-01 20:57:47 -0500

    background image now properly fits the entire browser screen

--- voyage/gulpfile.js
+++ voyage/gulpfile.js
@@ -1,32 +1,4 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var cssvars = require('postcss-simple-vars');
-var nested = require('postcss-nested')
-var cssImport = require('postcss-import');
-
-gulp.task('default', function() {
-  console.log("new gulp task");
-});
-
-gulp.task('html', function(){
-  console.log("insert new html");
-});
-
-gulp.task('styles', function(){
-  /*takes source style.css blueprint and automatically output
-  the complete styles.css in temp*/
-  return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-  .pipe(gulp.dest('./app/temp/styles'));
-});
-
-gulp.task('watch', function() {
-  watch('./app/index.html', function() {
-    gulp.start('html');
-  });
-  watch('./app/assets/styles/**/*.css', function() {
-    gulp.start('styles');
-  });
-});
+/*-------------------Import node modules & packages----------------------*/
+/*------------------------------------------------------------------------*/
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
