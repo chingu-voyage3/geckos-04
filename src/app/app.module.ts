c987b95d4671c0d092267a61f01f9059753ba3df
theodoredottavio <teddottavio@gmail.com>
2017-12-10 14:40:48 -0500

    intinailising weather feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { AppComponent } from './app.component';
 import { ClockComponent } from './clock/clock.component';
 import { ClockService } from './clock.service';
 import { CookieService } from 'ng2-cookies';
+import { WeatherComponent } from './weather/weather.component';
+import { WeatherService } from './weather.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClockComponent
+    ClockComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [CookieService, ClockService],
+  providers: [CookieService, ClockService, WeatherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
